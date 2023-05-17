cbf94639bdcfa26b0c50ba5058a47ae08440d045
Akin Evans <akinevans@gmail.com>
2023-05-16 19:04:27 -0700

    Add globe asset, update CSS variables, update font imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Button from "./components/Button";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navigation
+        link1='Home'
+        link2='Success stories'
+        link3='Pricing'
+      ></Navigation>
+    </div>
+  );
 }
 
 export default App;
